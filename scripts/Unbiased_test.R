cffe80894e818a33b47ff062239282edcf199e71
Cooperr18 <albertok.cooper@gmail.com>
2025-03-14 11:59:21 +0000

    Changes to probability validation and mutation
    First, for the unbiased_transmission function I've added validations for initial frequency and mutation probabilities so that they never return a negative or >1 values, and if it does, I get a warning and provide a default value (lines 18-27). I'm not 100% if this is the right solution, because it might be too arbitrary (?) and we're sort of interrupting the "natural course" of the simulation.
    Secondly, I've modified the conditionals of mutation from A to B and B to A to one single line using ifelse. So if the variant mutates (mutation = TRUE), if A = TRUE it will flip to B, and if A = FALSE (i.e. B) A will be returned (lines 43 - 50).

--- scripts/Unbiased_test.R
+++ scripts/Unbiased_test.R
@@ -1,42 +1,53 @@
-# USING THE TEST FOR UNBIAS-------------------------------------------------
+# UNBIASED TRANSMISSION PIPELINE ----------------------------------
 
 library(dplyr)
 library(signatselect)
 library(ggplot2)
+library(tibble)
 
-# install.packages("pak")
 pak::pkg_install("benmarwick/signatselect")
 
+# The parameters of the simulation are:
+#   N: Population size (integer)
+#   mu: Mutation rate (numeric, between 0 and 1, inclusive)
+#   p_0: Initial frequency of trait "A" (numeric, between 0 and 1, inclusive)
+#   t_max: Number of generations (integer)
+#   r_max: Number of replicate runs (integer)
+
 unbiased_mutation <- function(N, mu, p_0, t_max, r_max) {
+  # Validate p_0 and mu probabilities
+  # If sampling returns negative or >1, we warn and provide a default value
+  if(p_0 < 0 || p_0 > 1) {
+    warning("p_0 must be between 0 and 1 (inclusive).")
+    p_0 <- 0.5
+  }
+  if(mu < 0 || mu > 1) {
+    warning("mu must be between 0 and 1 (inclusive).")
+    mu <- 0.5
+  }
   # Create the output tibble
   output <- tibble(generation = rep(1:t_max, r_max), 
                    p = as.numeric(rep(NA, t_max * r_max)), 
                    run = as.factor(rep(1:r_max, each = t_max))) 
   
-  for (r in 1:r_max) { # Controls the number of runs in the experiment
+  for (r in 1:r_max) { # Loop over runs
+    # Initialize population of A and B based on p_0
     population <- tibble(trait = sample(c("A", "B"), N, replace = TRUE, 
                                         prob = c(p_0, 1 - p_0)))
-    # Add first generation's p for run r
+    # Initial frequency of A
     output[output$generation == 1 & output$run == r, ]$p <- 
       sum(population$trait == "A") / N 
-    for (t in 2:t_max) {
-      # Copy individuals to previous_population tibble
-      previous_population <- population 
+    
+    for (t in 2:t_max) { # Loop over generations
       
-      # Determine the probability of 'mutant' individuals
+      # Determine the probability of "mutant" individuals
+      # If they mutate, TRUE is returned
       mutate <- sample(c(TRUE, FALSE), N, prob = c(mu, 1 - mu), replace = TRUE) 
       
-      # If there are 'mutants' from A to B
-      if (nrow(population[mutate & previous_population$trait == "A", ]) > 0) { 
-        # Then flip them to B
-        population[mutate & previous_population$trait == "A", ]$trait <- "B" 
-      }
-      
-      # If there are 'mutants' from B to A
-      if (nrow(population[mutate & previous_population$trait == "B", ]) > 0) { 
-        # Then flip them to A
-        population[mutate & previous_population$trait == "B", ]$trait <- "A" 
-      }
+      # Only select those traits that mutate
+      population$trait[mutate] <- ifelse(population$trait[mutate] == "A", "B", "A")
+      # mutants which A = TRUE, B will be returned
+      # mutants which A = FALSE, A will be returned
       
       # Get p and put it into output slot for this generation t and run r
       output[output$generation == t & output$run == r, ]$p <- 
@@ -48,11 +59,11 @@ unbiased_mutation <- function(N, mu, p_0, t_max, r_max) {
 }
 
 # Parameters for simulation
-N <- 10000     # Population size
-mu <- 0.05    # Mutation rate
-p_0 <- 0.5    # Initial allele frequency
-t_max <- 150  # Number of generations
-r_max <- 1   # Number of replicate runs (use 1 to match `fit()` input format)
+N <- 10000     
+mu <- 0.05   
+p_0 <- 0.5    
+t_max <- 150  
+r_max <- 1  # 1 to match "fit()" input format
 
 
 sim_data <- unbiased_mutation(N, mu, p_0, t_max, r_max) # Simulate neutrality
@@ -67,3 +78,7 @@ neutral_test <- tibble(
 # Apply the fit() function
 fit_result <- fit(time = neutral_test$time, v = neutral_test$freq)
 print(fit_result)
+
+
+
+
